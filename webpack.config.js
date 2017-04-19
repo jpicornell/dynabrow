15472da5616efbd4285d5f8e13bdcf58143e5c8b
Joan <JoanPicornell@MacBook-Pro-de-Joan.local>
2017-04-19 12:08:57 +0200

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './lib/index.js',
   target: 'node',
   output: {
-    filename: 'bundle.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
